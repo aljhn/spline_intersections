ee89987949c51cb444008791a851f21635a0ec72
Albert Johannessen <albertjohannessen@live.no>
2026-08-11 01:19:50 +0200

    can now insert states in any order to trajectory

--- src/spline_intersections/trajectory.py
+++ src/spline_intersections/trajectory.py
@@ -27,31 +27,32 @@ class Trajectory:
 
     def add_state(self, state: State) -> None:
         self.states.append(state)
+        self.states.sort(key=lambda state: state.t)
+
+        self.segments.clear()
 
         if len(self.states) == 1:
-            t = self.states[-1].t
-            p = self.states[-1].p
-            v = self.states[-1].v
+            t = self.states[0].t
+            p = self.states[0].p
+            v = self.states[0].v
 
             coeffs = spline_get_linear(t, p, v)
             segment = Segment(t, coeffs)
             self.segments.append(segment)
 
-        elif len(self.states) == 2 and len(self.segments) == 1:
-            self.segments.clear()
+        else:
+            for i in range(len(self.states) - 1):
+                t0 = self.states[i].t
+                p0 = self.states[i].p
+                v0 = self.states[i].v
 
-        if len(self.states) >= 2:
-            t0 = self.states[-2].t
-            p0 = self.states[-2].p
-            v0 = self.states[-2].v
+                t1 = self.states[i + 1].t
+                p1 = self.states[i + 1].p
+                v1 = self.states[i + 1].v
 
-            t1 = self.states[-1].t
-            p1 = self.states[-1].p
-            v1 = self.states[-1].v
-
-            coeffs = spline_get_cubic(t0, p0, v0, t1, p1, v1)
-            segment = Segment(t0, coeffs)
-            self.segments.append(segment)
+                coeffs = spline_get_cubic(t0, p0, v0, t1, p1, v1)
+                segment = Segment(t0, coeffs)
+                self.segments.append(segment)
 
     def eval(self, t: float) -> float | np.ndarray:
         if len(self.segments) == 0:
@@ -116,7 +117,7 @@ class Trajectory:
 
             coeffs0_index = self.find_segment_index(t0)
             coeffs0 = self.segments[coeffs0_index].coeffs
- 
+
             coeffs1_index = other.find_segment_index(t1)
             coeffs1 = other.segments[coeffs1_index].coeffs
 
@@ -135,7 +136,7 @@ class Trajectory:
                 f = poly_norm_squared.eval(r)
 
                 if f < threshold:
-                    t_intersections.append(r) 
+                    t_intersections.append(r)
 
         t_intersections.sort()
 
@@ -145,4 +146,3 @@ class Trajectory:
                 t_unique.append(t)
 
         return t_unique
-
